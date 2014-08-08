8e9d5bf454c06c21c83e2600c5bbb27037c0dcc8
Philipp Schardt <philipp.schardt.1991@gmail.com>
2014-08-08 14:31:58 +0200

    cc

--- src/test/Test_CrossCorrelation.java
+++ src/test/Test_CrossCorrelation.java
@@ -9,23 +9,21 @@ import extractors.SingleFeatureExtractor;
 public class Test_CrossCorrelation {
 
 	private static ArrayList<SingleFeatureExtractor> extractorList;
-
+	private static float granularity = 0.1f;
+	
 	static {
 
 	}
 
 	public final static void main(int argc, String[] args) {
 
-		// parse Signal to Samples
+		// parse Signal to LableledFeatureVector
 		ArrayList<LabeledFeatureVector> trainSamples = null;
 		
-		// extract features
-		SignalToFeatureConverter converter = new SignalToFeatureConverter(extractorList);
-		
-		
 		// validate
-		new CrossCorrelation(trainSamples);
+		CrossCorrelation cc = new CrossCorrelation(trainSamples);
 
+		cc.crossValidation(granularity);
 	}
 
 }
