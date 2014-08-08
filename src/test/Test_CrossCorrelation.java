b0fb8f02a10e4ffb68a376cd01eabfc60a144150
Philipp Schardt <philipp.schardt.1991@gmail.com>
2014-08-08 14:33:31 +0200

    extractorList

--- src/test/Test_CrossCorrelation.java
+++ src/test/Test_CrossCorrelation.java
@@ -3,15 +3,47 @@ package test;
 import java.util.ArrayList;
 
 import data.LabeledFeatureVector;
+import extractors.MeanAbsoluteValue;
 import extractors.SignalToFeatureConverter;
+import extractors.SimpleSquareIntegral;
 import extractors.SingleFeatureExtractor;
+import extractors.SlopeSignChanges;
+import extractors.Variance;
+import extractors.WaveformLength;
+import extractors.WillsonAmplitude;
 
 public class Test_CrossCorrelation {
 
-	private static ArrayList<SingleFeatureExtractor> extractorList;
+	private static ArrayList<SingleFeatureExtractor> extractors;
 	private static float granularity = 0.1f;
-	
+
 	static {
+
+		extractors = new ArrayList<SingleFeatureExtractor>();
+
+		// MAV
+		MeanAbsoluteValue mav = new MeanAbsoluteValue();
+		extractors.add(mav);
+
+		// WAMP
+		WillsonAmplitude wamp = new WillsonAmplitude();
+		extractors.add(wamp);
+
+		// Varaince
+		Variance v = new Variance();
+		extractors.add(v);
+
+		// WaveformLength
+		WaveformLength wfl = new WaveformLength();
+		extractors.add(wfl);
+
+		// SlopeSignChanges
+		SlopeSignChanges ssc = new SlopeSignChanges();
+		extractors.add(ssc);
+
+		// SimpleSquareIntegral
+		SimpleSquareIntegral ssi = new SimpleSquareIntegral();
+		extractors.add(ssi);
 
 	}
 
@@ -19,7 +51,7 @@ public class Test_CrossCorrelation {
 
 		// parse Signal to LableledFeatureVector
 		ArrayList<LabeledFeatureVector> trainSamples = null;
-		
+
 		// validate
 		CrossCorrelation cc = new CrossCorrelation(trainSamples);
 
